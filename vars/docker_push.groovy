5e90b90861dbebadb363b94840499f70eb0b73f9
raxj06 <160596209+raxj06@users.noreply.github.com>
2026-01-05 17:08:57 +0530

    Update Docker image tagging and pushing logic

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,8 +4,8 @@ def call(String ProjectName, String ImageTag, String DockerHubUser){
                     passwordVariable:"DockerHubPass",
                     usernameVariable:"DockerHubUser")]){
                 sh "docker login -u ${DockerHubUser} -p ${DockerHubPass}"
-                sh "docker image tag notes-app:latest ${DockerHubUser}/:latest"
-                sh "docker push ${DockerHubUser}/notes-app:latest"
+                sh "docker image tag ${ProjectName}:${ImageTag} ${DockerHubUser}/:latest"
+                sh "docker push ${DockerHubUser}/${ProjectName}:${ImageTag}"
                 
    }
 }
